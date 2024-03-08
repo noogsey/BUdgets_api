9624f404afa8ce5b11b629bf935463f971c059be
noogsey <155902888+noogsey@users.noreply.github.com>
2024-03-08 19:36:08 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,19 +1,23 @@
+"""
+URL configuration for djapp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path
-from djapi import views
-from .views import ExpenseListCreateView, ExpenseRetrieveUpdateDestroyView, BudgetListCreateView, BudgetRetrieveUpdateDestroyView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path,include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('expenses/', ExpenseListCreateView.as_view(), name='expense-list-create'),
-    path('expenses/<int:pk>/', ExpenseRetrieveUpdateDestroyView.as_view(), name='expense-retrieve-update-destroy'),
-    path('budgets/', BudgetListCreateView.as_view(), name='budget-list-create'),
-    path('budgets/<int:pk>/', BudgetRetrieveUpdateDestroyView.as_view(), name='budget-retrieve-update-destroy'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-
+    path('djapi/', include('djapi.urls')),
 ]
